c172f9e7f987568c2f260cf5c91ca4b9405b534c
lnam42318-design <lnam42318@gmail.com>
2026-07-04 20:58:58 +0700

    Update SamuraiMod.java

--- src/main/java/com/example/samuraimod/SamuraiMod.java
+++ src/main/java/com/example/samuraimod/SamuraiMod.java
@@ -1,34 +1,96 @@
 package com.example.samuraimod;
 
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
 
-@Mod(SamuraiMod.MODID)
-public class SamuraiMod {
+import java.util.EnumMap;
+import java.util.function.Supplier;
 
-    public static final String MODID = "samuraimod";
+public enum SamuraiArmorMaterial implements ArmorMaterial {
 
-    public SamuraiMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    SAMURAI("samurai", 22,
+            new EnumMap<>(java.util.Map.of(
+                    ArmorItem.Type.BOOTS, 3,
+                    ArmorItem.Type.LEGGINGS, 5,
+                    ArmorItem.Type.CHESTPLATE, 7,
+                    ArmorItem.Type.HELMET, 3
+            )),
+            18,
+            SoundEvents.ARMOR_EQUIP_IRON,
+            2.5f,
+            0.1f,
+            () -> Ingredient.of(Items.IRON_INGOT));
 
-        // Register our deferred registers
-        ModItems.ITEMS.register(modEventBus);
+    private final String name;
+    private final int durabilityMultiplier;
+    private final EnumMap<ArmorItem.Type, Integer> protectionValues;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredient;
 
-        modEventBus.addListener(this::addCreative);
+    SamuraiArmorMaterial(String name, int durabilityMultiplier,
+                          EnumMap<ArmorItem.Type, Integer> protectionValues,
+                          int enchantmentValue, SoundEvent sound,
+                          float toughness, float knockbackResistance,
+                          Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionValues = protectionValues;
+        this.enchantmentValue = enchantmentValue;
+        this.sound = sound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
     }
 
-    // Add our items to the Combat creative tab so they show up in the inventory
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
-            event.accept(ModItems.SAMURAI_KATANA);
-            event.accept(ModItems.SAMURAI_HELMET);
-            event.accept(ModItems.SAMURAI_CHESTPLATE);
-            event.accept(ModItems.SAMURAI_LEGGINGS);
-            event.accept(ModItems.SAMURAI_BOOTS);
-        }
+    @Override
+    public int getDurabilityForType(ArmorItem.Type type) {
+        return switch (type) {
+            case HELMET -> 11 * durabilityMultiplier;
+            case CHESTPLATE -> 16 * durabilityMultiplier;
+            case LEGGINGS -> 15 * durabilityMultiplier;
+            case BOOTS -> 13 * durabilityMultiplier;
+        };
+    }
+
+    @Override
+    public int getDefenseForType(ArmorItem.Type type) {
+        return protectionValues.get(type);
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return enchantmentValue;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return sound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return SamuraiMod.MODID + ":" + name;
+    }
+
+    @Override
+    public float getToughness() {
+        return toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return knockbackResistance;
     }
 }
